2accf0d21ffd37a25c33c13d0d7a25977ab52201
laravellogicloop <laravellogicloop@gmail.com>
2024-08-29 02:55:06 +0500

    added todo list delete functionality and completed remaining css styling.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const todoList = [];
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2024-8-29'
+}, {
+    name: 'wash dishes',
+    dueDate: '2024-8-29'
+}];
 
 function renderTodoList() {
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name, dueDate } = todoObject;
+        const html = `
+        <div class="todo-list-li">
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button class="delete-btn" onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+                ">Delete
+            </button>
+            </div>
+        </div>`;
         todoListHTML += html;
     }
 
@@ -19,7 +38,13 @@ function addTask() {
     const inputElement = document.querySelector('.name-input');
     const name = inputElement.value;
 
-    todoList.push(name);
+    const dateInput = document.querySelector('.due-date-input');
+    const dueDate = dateInput.value;
+
+    todoList.push({
+        name: name,
+        dueDate: dueDate
+    });
 
     inputElement.value = '';
 
