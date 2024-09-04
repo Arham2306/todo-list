33cef6f3793993aa7e12d4f466136664e9e901ef
Arham2306 <arhamfarooqui777@gmail.com>
2024-09-05 00:16:27 +0500

    refined the code with adding event listeners

--- index.js
+++ index.js
@@ -21,15 +21,14 @@ function renderTodoList() {
         <div class="todo-list-li">
             <div>${name}</div>
             <div>${dueDate}</div>
-            <button class="delete-btn" onclick="
-                todoList.splice(${index}, 1);
-                renderTodoList();
-                ">Delete
+            <button class="delete-btn delete-todo-btn">
+                Delete
             </button>
             </div>
         </div>`;
         todoListHTML += html;
     })
+
 
     // for (let i = 0; i < todoList.length; i++) {
     //     const todoObject = todoList[i];
@@ -51,10 +50,21 @@ function renderTodoList() {
     // }
 
     document.querySelector('.todo-list-output').innerHTML = todoListHTML;
+
+    document.querySelector('.todo-list-li').innerHTML = todoListHTML;
+    document.querySelectorAll('.delete-todo-btn')
+        .forEach((deleteButton, index)=>{
+            deleteButton.addEventListener('click', () => {
+                todoList.splice(index, 1);
+                renderTodoList();
+            });
+        });
 }
 
 
-
+document.querySelector('.add-todo-btn').addEventListener('click', () => {
+    addTask();
+})
 
 function addTask() {
     const inputElement = document.querySelector('.name-input');
